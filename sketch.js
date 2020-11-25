ed45673fc7cbddfada46bb4859230194887ad290
Noah Ortiz <61887711+noaho1@users.noreply.github.com>
2020-11-25 15:33:13 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var car, wall;
+var speed, weight;
+
+function setup() {
+  createCanvas(1600, 400);
+
+  car = createSprite(50, 200, 50, 50)
+  wall = createSprite(1500, 200, 60, height / 2);
+  wall.shapeColor = "black"
+  speed = random(55, 90);
+  weight = random(400, 1500);
+
+  car.velocityX = speed;
+
+}
+
+function draw() {
+  background(255, 255, 255);
+
+  if (wall.x - car.x < (car.width + wall.width) / 2) {
+
+    car.velocityX = 0;
+    var deformation = (0.5 * weight * speed * speed) / 22500;
+    if (deformation < 100) {
+      car.shapeColor = "green";
+
+    } else if (deformation > 100 && deformation < 180) {
+      car.shapeColor = "yellow";
+    } else if (deformation > 180) {
+      car.shapeColor = "red";
+    }
+
+  }
+
+  drawSprites();
+
+}
